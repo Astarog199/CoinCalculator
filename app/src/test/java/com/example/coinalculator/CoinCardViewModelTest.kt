79300b4c2ed9bfc1480e836c046029fc5271e9fa
pkn <PKN199@yandex.ru>
2024-08-17 13:40:26 +0300

    add test for CoinCardViewModel

--- app/src/test/java/com/example/coinalculator/CoinCardViewModelTest.kt
+++ app/src/test/java/com/example/coinalculator/CoinCardViewModelTest.kt
@@ -4,9 +4,13 @@ import com.example.coinalculator.ui.coins.domain.AddFavoriteUseCase
 import com.example.coinalculator.ui.coins.domain.CoinDetails
 import com.example.coinalculator.ui.coins.domain.ConsumeCoinCard
 import com.example.coinalculator.ui.coins.presently.card.CoinCardViewModel
+import com.example.coinalculator.ui.coins.presently.card.states.CoinCardStates
 import com.example.coinalculator.ui.coins.presently.card.states.CoinDetailsStatesMapper
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -14,6 +18,7 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.whenever
+import org.mockito.stubbing.OngoingStubbing
 
 @RunWith(MockitoJUnitRunner::class)
 class CoinCardViewModelTest {
@@ -47,7 +52,7 @@ class CoinCardViewModelTest {
     @Test
     fun `requestCoinCard WHEN starting loading EXPECT isLoading flag in state`(){
         //arrange
-        whenever(consumeCoinCard.invoke("3")).thenReturn(flowOf(loadCoin()))
+        whenever(consumeCoinCard.invoke("3")).thenReturn(flowOf())
 
         //act
         sut.loadCoinCard()
@@ -56,15 +61,40 @@ class CoinCardViewModelTest {
         Assert.assertTrue(sut.state.value.isLoading)
     }
 
+    @Test
+    fun `requestCoinCard WHEN product is loaded EXPECT reset isLoading flag and set product state`(){
+        //arrange
+        whenever(consumeCoinCard.invoke("3")).thenReturn(flowOf(loadCoin()))
+
+        //act
+        sut.loadCoinCard()
+
+        //assert
+        Assert.assertFalse(sut.state.value.isLoading)
+
+    }
+
+    @Test
+    fun `requestCoinCard WHEN product loading has error EXPECT state has en error`() {
+        // arrange
+        whenever(consumeCoinCard.invoke("2")).thenReturn(flow { throw IllegalStateException() })
+
+        // act
+        sut.loadCoinCard()
+
+        // assert
+        Assert.assertTrue(sut.state.value.hasError)
+    }
+
+
     private fun loadCoin() : CoinDetails {
         return CoinDetails (
-            id = 0,
-            name = "",
-            market  = "",
-            price  = "",
+            id = 3,
+            name = "BTC",
+            market  = "Binance",
+            price  = "60000",
             change24h  = 0f,
             isFavorite = false
         )
     }
-
 }
