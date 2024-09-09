f344e07e13b4e88442782e01e4dc456bd965f6dc
pkn <PKN199@yandex.ru>
2024-09-09 15:53:01 +0300

    Update commonRepository, added a function to update values in the list

--- app/src/main/java/com/example/coinalculator/ui/common/data/CommonRepositoryImpl.kt
+++ app/src/main/java/com/example/coinalculator/ui/common/data/CommonRepositoryImpl.kt
@@ -4,9 +4,13 @@ import com.example.coinalculator.ui.common.data.room.Entity
 import com.example.coinalculator.ui.common.data.room.NewCoin
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class CommonRepositoryImpl(
@@ -17,6 +21,20 @@ class CommonRepositoryImpl(
 ) {
     private val scope = CoroutineScope(SupervisorJob() + coroutineDispatcher)
     private var newList: List<CoinsDto> = mutableListOf()
+    private lateinit var refreshTimer: Job
+
+    init {
+        refresh()
+    }
+
+    private fun refresh() {
+        refreshTimer = scope.launch(Dispatchers.Default) {
+            while (true) {
+                requestListFromApiService()
+                delay(60000L)// 60 seconds
+            }
+        }
+    }
 
     private suspend fun requestListFromApiService() {
         newList = coinsRemoteDataSource.getList()
@@ -24,8 +42,6 @@ class CommonRepositoryImpl(
 
     private fun saveList() {
         scope.launch {
-            requestListFromApiService()
-
             val coins = newList.map(coinsDataMapper::toEntity)
             coinsLocalDataSource.saveMany(
                 coins.map { coin ->
@@ -54,29 +70,33 @@ class CommonRepositoryImpl(
                 if (coins.isEmpty()) {
                     saveList()
                 } else {
-                    coins.map { coin ->
-                        for (i in newList) {
-                            if (i.name == coin.name) {
-                                coinsLocalDataSource.updateCoin(
-                                    Entity(
-                                        name = coin.name,
-                                        image = i.image,
-                                        price = i.currentPrice.toString(),
-                                        price_percentage_change_24h = i.priceChangePercentage24h,
-                                        price_change_24h = i.priceChange24h,
-                                        market_cap = i.marketCap,
-                                        market_cap_rank = i.marketCapRank,
-                                        total_volume = i.totalVolume,
-                                        isFavorite = coin.isFavorite
-                                    )
-                                )
-                            }
-                        }
-                    }
+                    updateValueForList(coins)
                 }
             }
         }
         return coinsLocalDataSource.consume().flowOn(coroutineDispatcher)
+    }
+
+    private suspend fun updateValueForList(list: List<Entity>) {
+        list.map { coin ->
+            for (i in newList) {
+                if (i.name == coin.name) {
+                    coinsLocalDataSource.updateCoin(
+                        Entity(
+                            name = coin.name,
+                            image = i.image,
+                            price = i.currentPrice.toString(),
+                            price_percentage_change_24h = i.priceChangePercentage24h,
+                            price_change_24h = i.priceChange24h,
+                            market_cap = i.marketCap,
+                            market_cap_rank = i.marketCapRank,
+                            total_volume = i.totalVolume,
+                            isFavorite = coin.isFavorite
+                        )
+                    )
+                }
+            }
+        }
     }
 
     suspend fun changeFavorite(coin: Entity) {
